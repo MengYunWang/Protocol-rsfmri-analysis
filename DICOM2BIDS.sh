15e3c705f6d942b125f904bfce745e9f86285106
Meng-Yun Wang <mengyun.wang@uib.no>
2022-03-30 16:38:29 +0200

    Add files via upload

---
+++ DICOM2BIDS.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# Created by M.Y. WANG
+
+
+# Organize the data into BIDS format
+
+filepath=/Users/wang/Desktop/Research_projects/BBSC/Functional/Data
+cd $filepath
+mkdir ./Reorganized/BIDS
+
+ls ./Raw_data | grep -i sub- > ./Raw_data/subjList.txt # command grep is trying to filter the input with 'sub-'. -i ignore the high or low cases 
+
+for sub in `cat ./Raw_data/subjList.txt`; do 
+    mkdir ./Reorganized/BIDS/$sub
+    ls ./Raw_data/$sub > ./Raw_data/sesList.txt #list files with 'ses-' in sub directories to sesList.txt 
+    ses_num=0
+    for ses_name in `cat ./Raw_data/sesList.txt`; do
+        
+        ((ses_num=$ses_num+1)) 
+        ses_num=$(printf "%02d" $ses_num)
+        # ses_num=ses_num+1 | printf "%02d " 
+        sespath=./Reorganized/BIDS/$sub/ses-${ses_num}
+        
+        mkdir $sespath
+        mkdir $sespath/anat
+        mkdir $sespath/func
+
+
+        dcm2niix -b y -z y -f ${sub}_ses-${ses_num}_T1W -o $sespath/anat/ ./Raw_data/$sub/$ses_name/*_Sag_FSPGR_3D/ 
+
+        dcm2niix -b y -z y -f ${sub}_ses-${ses_num}_task-rest_bold -o $sespath/func/ ./Raw_data/$sub/$ses_name/*_fMRI_default_restate/
+        cd ./$sespath/func/   
+        echo "$(jq '. += {"TaskName": "rest"}' ${sub}_ses-${ses_num}_task-rest_bold.json)" > ${sub}_ses-${ses_num}_task-rest_bold.json
+        
+        cd $filepath
+    done
+    
+done
+
+
+# check the quality of the data
+
+# docker run -it --rm -v /Users/wang/Desktop/UiB/Research_projects/BBSC/Functional/Data/Reorganized/BIDS:/data:ro \
+#                    -v /Users/wang/Desktop/UiB/Research_projects/BBSC/Functional/QC/Report_all:/out \
+#                    poldracklab/mriqc:latest /data /out participant
+
+#docker run -it --rm -v /Users/wang/Desktop/UiB/Research_projects/BBSC/Functional/Data/Reorganized/BIDS:/data:ro \
+#                    -v /Users/wang/Desktop/UiB/Research_projects/BBSC/Functional/QC/Report_all:/out \
+#                   poldracklab/mriqc:latest /data /out group
+
