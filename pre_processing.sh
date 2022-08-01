4278b25b3d711c3446d01d97f302fe8dcf2e885d
Meng-Yun Wang <mengyun.wang@uib.no>
2022-08-01 23:00:16 +0200

    Update pre_processing.sh

--- pre_processing.sh
+++ pre_processing.sh
@@ -7,13 +7,13 @@
 
 filepath=/Users/wang/Desktop/Research_projects/BBSC/Functional/
 cd $filepath
-# mkdir ./Pre_processing
+# mkdir -p ./Pre_processing
 
 ls ./Data/Reorganized/All | grep -i sub- > ./Pre_processing/subjList.txt # command grep is trying to filter the input with 'sub-'. -i ignore the high or low cases 
 
 for sub in `cat ./Pre_processing/subjList.txt`; do
 
-    mkdir ./Pre_processing/$sub
+    mkdir -p ./Pre_processing/$sub
     ls ./Data/Reorganized/All/$sub > ./Pre_processing/$sub/sesList.txt #list files with 'ses-' in sub directories to sesList.txt 
     # ses_num=0
     
@@ -21,9 +21,11 @@ for sub in `cat ./Pre_processing/subjList.txt`; do
      echo "===> Starting processing of $sub $ses_name"
      #((ses_num=$ses_num+1)) 
      mkdir ./Pre_processing/$sub/$ses_name
+     
      echo "Brain extraction..." 
      bet2 $filepath/Data/Reorganized/All/$sub/$ses_name/anat/${sub}_${ses_name}_T1w \
             $filepath/Pre_processing/$sub/$ses_name/${sub}_${ses_name}_T1w_brain  -f 0.8 -g 0 -c 93 119 155 
+        
         # If the brain mask doesn’t exist, create it
         #if [ ! -f anat/${subj}_T1w_brain.nii.gz ]; then
         #    echo "Skull-stripped brain not found, creating it..."
@@ -31,10 +33,8 @@ for sub in `cat ./Pre_processing/subjList.txt`; do
         #       $filepath/All/sub-1/ses-1/anat/sub-1_ses-1_T1w_brain  -f 0.8 -g 0 -c 93 119 155
         #fi
      
-
      # Note that we are using the | character to delimit the patterns
-     # instead of the usual / character because there are / characters
-     # in the pattern.
+     # instead of the usual / character because there are / characters in the pattern.
      
      sed -i '' "s|sub-1|${sub}|g" ./Pre_processing/rs_prep.fsf
      sed -i '' "s|ses-1|${ses_name}|g" ./Pre_processing/rs_prep.fsf
@@ -42,7 +42,6 @@ for sub in `cat ./Pre_processing/subjList.txt`; do
      # Now everything is set up to run feat
      echo "===> Starting to run feat"
      feat ./Pre_processing/rs_prep.fsf
+     echo "===>Finished! Now next..."
     done
-    # Go back to the initial directory, and repeat the sub loop
-
 done
